8bbd824b5b4af84a597a7c5f716ba48683bb9d67
PRAFUL KODIYATAR <prafulkodiyatar@gmail.com>
2022-07-30 12:48:07 +0530

    Added gitignore

--- backend/app.js
+++ backend/app.js
@@ -1,12 +1,14 @@
 const express = require('express');
 const app = express();
+const errorMiddleware = require('./middleware/error');
 
-app.use(express.json())
+app.use(express.json());
 
 //Route Imports
 const product = require('./routes/productRoute');
-app.use("/api/v1",product);
+app.use('/api/v1', product);
 
-
+//middleware for Error
+app.use(errorMiddleware);
 
 module.exports = app;
--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,77 +1,60 @@
-const Product = require("../models/productModel");
+const Product = require('../models/productModel');
+const errorHander = require('../utils/errorHander');
 
-
-
-
-//create product -- Admin 
-exports.createProduct = async(req,res,next)=>{
-    const product = await Product.create(req.body);
-    res.status(200).json({
-        success:true,
-        product
-    })
-}
-
+//create product -- Admin
+exports.createProduct = async (req, res, next) => {
+  const product = await Product.create(req.body);
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
 
 //get all products
-exports.getAllProducts = async(req,res)=>{
-    const products = await Product.find();
-    res.status(200).json({success:true,products})
-}
+exports.getAllProducts = async (req, res) => {
+  const products = await Product.find();
+  res.status(200).json({ success: true, products });
+};
 
 //productDetails
-exports.productDetails = async(req,res)=>{
-    const product = await Product.findById(req.params.id);
-    if(!product)
-    {
-        return res.status(500).json({
-            success:false,
-            massage:"Product not Found"
-        })
-    }
-    res.status(200).json({
-        success:true,
-        product
-    })
-}
-
+exports.productDetails = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new errorHander('product not found', 404));
+  }
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
 
 //update product -admin
-exports.updateProduct = async(req,res,next)=>{
-    let product = Product.findById(req.params.id);
-    if(!product)
-    {
-        return res.status(500).json({
-            success:false,
-            massage:"Product not Found"
-        })
-    }
-    product = await Product.findByIdAndUpdate(req.params.id,req.body,{
-        new:true,
-        runValidators:true,
-        useFindAndModify:false 
-    });
-    res.status(200).json({
-        success:true,
-        product
-    })
-}
-
+exports.updateProduct = async (req, res, next) => {
+  let product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new errorHander('product not found'), 404);
+  }
+  product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+    runValidators: true,
+    useFindAndModify: false,
+  });
+  res.status(200).json({
+    success: true,
+    product,
+  });
+};
 
 //delete product --admin
 
-exports.deleteProduct = async(req,res,next)=>{
-    const product = await Product.findById(req.params.id);
-    if(!product)
-    {
-        return res.status(500).json({
-            success:false,
-            massage:"product not found"
-        })
-    }
-    await product.remove();
-    res.status(200).json({
-        success:true,
-        massage:"product deleted successfully"
-    })
-}
+exports.deleteProduct = async (req, res, next) => {
+  const product = await Product.findById(req.params.id);
+  if (!product) {
+    return next(new errorHander('product not found', 404));
+  }
+  await product.remove();
+  res.status(200).json({
+    success: true,
+    massage: 'product deleted successfully',
+  });
+};
